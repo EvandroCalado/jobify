1d8cb3b1d6c9370153587c738a689292afdbbfeb
Evandro Calado <evandrocalado07@gmail.com>
2024-02-12 23:22:02 -0300

    ✨ feat(router): create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
 export const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
