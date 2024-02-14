608ca974dc9b474cd2e58fbe376ad322af7f2f0e
Evandro Calado <evandrocalado07@gmail.com>
2024-02-14 00:07:44 -0300

    ✨ feat(pages): create login

---
+++ src/pages/Login/Login.styles.ts
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+import { Wrapper as WrapperRegister } from "../Register/Register.styles";
+
+export const Wrapper = styled(WrapperRegister)``;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router-dom";
+import { Input, Logo } from "../../components";
+import * as Styled from "./Login.styles";
+
 export const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Styled.Wrapper>
+      <form className="form">
+        <Logo />
+
+        <h4>login</h4>
+
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          defaultValue="guest@gmail.com"
+          label="email"
+        />
+
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          defaultValue="secret"
+          label="password"
+        />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+
+        <button type="button" className="btn btn-block">
+          Guest User
+        </button>
+
+        <p>
+          Not a member yet ?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Styled.Wrapper>
   );
 };
