68199db44f2157382d355bf4a0f2e7967e9ddb94
Evandro Calado <evandrocalado07@gmail.com>
2024-02-13 21:16:32 -0300

    ✨ feat(pages): create error

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,7 +1,26 @@
+import { ErrorResponse, Link, useRouteError } from "react-router-dom";
+import * as Styled from "./Error.styles";
+
 export const Error = () => {
+  const error = useRouteError() as ErrorResponse;
+
+  if (error.status === 404) {
+    return (
+      <Styled.Wrapper>
+        <img src="./src/assets/not-found.svg" alt="Error" />
+
+        <h3>Ohh! page not found</h3>
+
+        <p>we can't seem to find the page you are loking for</p>
+
+        <Link to="/dashboard">back home</Link>
+      </Styled.Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error</h1>
-    </div>
+    <Styled.Wrapper>
+      <h3>something went wrong</h3>
+    </Styled.Wrapper>
   );
 };
