79cd286dee80f449413baab924a7b66a37f4e29f
Evandro Calado <evandrocalado07@gmail.com>
2024-02-14 14:56:47 -0300

    🐳 chore(story): add story book

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import { Logo } from ".";
+
+export default {
+  title: "Components/Logo",
+  component: Logo,
+} as Meta;
+
+export const Default = () => <Logo />;
