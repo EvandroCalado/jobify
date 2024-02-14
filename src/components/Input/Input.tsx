4cc4c9f04e0bb6bb891e570934e19da8bdab606b
Evandro Calado <evandrocalado07@gmail.com>
2024-02-14 00:08:05 -0300

    ✨ feat(components): create input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,16 @@
+import { FC, InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+export const Input: FC<InputProps> = ({ label, ...rest }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={label} className="form-label">
+        {label}
+      </label>
+      <input className="form-input" {...rest} />
+    </div>
+  );
+};
