0cdb77dcae4179dcc0b3aac10abb6558c4f0cbe9
Evandro Calado <evandrocalado07@gmail.com>
2024-02-15 23:28:51 -0300

    ✨ feat(components): create navlinks

---
+++ src/components/NavLinks/NavLinks.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { NavLink } from "react-router-dom";
+import { useDashboardContext } from "../../hooks/useDashboardContext";
+import { links } from "../../utils/links";
+
+export interface NavLinksProps {
+  isBigSidebar?: boolean | undefined;
+}
+
+export const NavLinks: FC<NavLinksProps> = ({ isBigSidebar }) => {
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <nav className="nav-links">
+      {links.map((link) => (
+        <NavLink
+          to={link.path}
+          key={link.text}
+          className="nav-link"
+          onClick={isBigSidebar ? undefined : toggleSidebar}
+          end
+        >
+          <span className="icon">{link.icon}</span>
+          {link.text}{" "}
+        </NavLink>
+      ))}
+    </nav>
+  );
+};
