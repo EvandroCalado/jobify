511e06b955f02aa6ea85d24f531e9c0bf9202d2e
Evandro Calado <evandrocalado07@gmail.com>
2024-02-16 20:22:20 -0300

    🌈 style(components): move styles
    move styles for nav links component

--- src/components/Sidebar/Sidebar.styles.ts
+++ src/components/Sidebar/Sidebar.styles.ts
@@ -33,35 +33,6 @@ export const Wrapper = styled.div`
         padding-left: 2.5rem;
       }
 
-      .nav-links {
-        padding-top: 2rem;
-        display: flex;
-        flex-direction: column;
-      }
-
-      .nav-link {
-        display: flex;
-        align-items: center;
-        color: ${theme.colors.textSecondaryColor};
-        padding: 1rem 0;
-        padding-left: 2.5rem;
-        text-transform: capitalize;
-        transition: padding-left 0.3s ease-in-out;
-      }
-
-      .nav-link:hover {
-        padding-left: 3rem;
-        color: ${theme.colors.primary500};
-        transition: ${theme.transitions.fast};
-      }
-
-      .icon {
-        font-size: 1.5rem;
-        margin-right: 1rem;
-        display: grid;
-        place-items: center;
-      }
-
       .active {
         color: ${theme.colors.primary500};
       }
