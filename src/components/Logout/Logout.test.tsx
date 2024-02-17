e2d272a09afa60a044d7f19032d7feba4ae273ab
Evandro Calado <evandrocalado07@gmail.com>
2024-02-17 00:20:24 -0300

    🧪 test(components): add test

---
+++ src/components/Logout/Logout.test.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, screen } from "@testing-library/react";
+import { Logout } from ".";
+import { renderTheme } from "../../styles/render-theme";
+
+describe("<Logout />", () => {
+  it("should render default", () => {
+    renderTheme(<Logout />);
+
+    expect(screen.getByRole("button", { name: "Guest" })).toBeInTheDocument();
+    expect(screen.getByTestId("dropdown")).toBeInTheDocument();
+    expect(screen.getByTestId("dropdown")).toHaveStyle({
+      visibility: "hidden",
+    });
+  });
+
+  it("should render visibility visible", () => {
+    renderTheme(<Logout />);
+
+    const button = screen.getByRole("button", { name: "Guest" });
+
+    fireEvent.click(button);
+
+    expect(screen.getByTestId("dropdown")).toHaveStyle({
+      visibility: "visible",
+    });
+  });
+});
