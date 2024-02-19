4229e08929dae9039a429983b02bab6fe0e6f642
Evandro Calado <evandrocalado07@gmail.com>
2024-02-18 23:16:36 -0300

    🐞 fix(main): rm theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import { App } from "./App.tsx";
 import { DashboardProvider } from "./contexts/DashboardProvider.tsx";
-import { GlobalStyles } from "./styles/global-styles.ts";
-import { theme } from "./styles/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DashboardProvider>
-      <ThemeProvider theme={theme}>
-        <App />
-        <GlobalStyles />
-      </ThemeProvider>
+      <App />
     </DashboardProvider>
   </React.StrictMode>
 );
