13d303a5bf221fb78e84fafaef5e1e10311a1790
Evandro Calado <evandrocalado07@gmail.com>
2024-02-26 17:50:42 -0300

    🐳 chore(stories): add decorators
    add decorators for context state change

--- src/components/Sidebar/Sidebar.stories.tsx
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -1,12 +1,21 @@
 import { Meta } from "@storybook/react";
 import { Sidebar } from ".";
+import { DashboardContext } from "../../contexts/DashboardProvider";
+import { useDashboardContext } from "../../hooks/useDashboardContext";
 
 export default {
   title: "Components/Sidebar",
-  component: Sidebar,
-  parameters: {
-    layout: "fullscreen",
-  },
+  decorators: [
+    (Story) => {
+      const data = useDashboardContext();
+
+      return (
+        <DashboardContext.Provider value={{ ...data, showSidebar: true }}>
+          <Story />
+        </DashboardContext.Provider>
+      );
+    },
+  ],
 } as Meta;
 
 export const Default = () => <Sidebar />;
--- src/components/SmallSidebar/SmallSidebar.stories.tsx
+++ src/components/SmallSidebar/SmallSidebar.stories.tsx
@@ -1,12 +1,22 @@
 import { Meta } from "@storybook/react";
 import { SmallSidebar } from ".";
+import { DashboardContext } from "../../contexts/DashboardProvider";
+import { useDashboardContext } from "../../hooks/useDashboardContext";
 
 export default {
   title: "Components/SmallSidebar",
   component: SmallSidebar,
-  parameters: {
-    layout: "fullscreen",
-  },
+  decorators: [
+    (Story) => {
+      const data = useDashboardContext();
+
+      return (
+        <DashboardContext.Provider value={{ ...data, showSidebar: true }}>
+          <Story />
+        </DashboardContext.Provider>
+      );
+    },
+  ],
 } as Meta;
 
 export const Default = () => <SmallSidebar />;
