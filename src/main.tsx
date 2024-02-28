ce71769978a08130e63c1dcb7f8615ca25941f24
Evandro Calado <evandrocalado07@gmail.com>
2024-02-27 21:12:01 -0300

    🐳 chore(main): remove provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
-import { DashboardProvider } from "./contexts/DashboardProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <DashboardProvider>
-      <App />
-    </DashboardProvider>
+    <App />
   </React.StrictMode>
 );
