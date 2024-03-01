9e7ad20a7c17f319c58575e6f760c07b9f136bcc
Evandro Calado <evandrocalado07@gmail.com>
2024-03-01 10:10:15 -0300

    🐳 chore(utils): create constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const JOB_STATUS = {
+  OPTION1: "pending",
+  OPTION2: "interview",
+  OPTION3: "declined",
+};
+
+export const JOB_TYPE = {
+  OPTION1: "full-time",
+  OPTION2: "pert-time",
+  OPTION3: "internship",
+};
+
+export const JOB_SORT_BY = {
+  OPTION1: "newest",
+  OPTION2: "oldest",
+  OPTION3: "a-z",
+  OPTION4: "z-a",
+};
